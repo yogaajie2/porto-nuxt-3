d81ad59524dc36226ea9a652812f4322474bdc6e
Yoga Prasetya <yogaajie2@gmail.com>
2023-11-17 17:53:05 +0700

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devServer: { port: 8000 },
+  devtools: { enabled: true },
+  typescript: { shim: false },
+});
